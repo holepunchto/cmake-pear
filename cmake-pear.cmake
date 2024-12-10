141501eb85d78ad4c7cee4fdeecb7dd0e6988819
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-12-10 14:21:03 +0100

    Bump prebuilds

--- cmake-pear.cmake
+++ cmake-pear.cmake
@@ -261,7 +261,7 @@ function(add_pear_appling target)
     SOURCE excdougxjday9q8d13azwwjss8p8r66fhykb18kzjfk9bwaetkuo
     DESTINATION "${prebuilds}"
     PREFIX /${host}
-    CHECKOUT 73
+    CHECKOUT 79
   )
 
   if(NOT TARGET c++)
