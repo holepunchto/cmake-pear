2bd9d78c42a28de0d3e37b476ba5a38f5752bf3a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-10-27 11:27:13 +0100

    Pin `libjstl`

--- cmake-pear.cmake
+++ cmake-pear.cmake
@@ -250,6 +250,7 @@ function(add_pear_appling target)
   bare_target(host)
 
   fetch_package("github:holepunchto/bare#f8cb4af")
+  fetch_package("github:holepunchto/libjstl#098664c")
   fetch_package("github:holepunchto/libappling#c8d995f")
   fetch_package("github:holepunchto/libfx#33678eb")
   fetch_package("github:holepunchto/libpear#f39cec0")
